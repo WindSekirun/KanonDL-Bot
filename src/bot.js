999dd5b9c23e9e24d53006343ef5d18c536e8478
Pyxis <pyxis@uzuki.live>
2019-10-06 04:38:35 +0900

    fix chatId issue

--- src/bot.js
+++ src/bot.js
@@ -68,28 +68,35 @@ promise.callbackQuery()
 
         if (callback.includes('1002-')) {
             bot.deleteMessage(chatId, model.data.message.message_id)
-            bot.sendMessage(chatId, message.ackready, removeKeyboard);
+            bot.sendMessage(chatId, message.ackreadyaudio, removeKeyboard);
 
             let url = callback.replace('1002-', '')
-            return wrapper.downloadAudio(url)
+            return wrapper.downloadAudio(chatId, url)
         } else if (callback.includes('1001-')) {
             bot.deleteMessage(chatId, model.data.message.message_id)
-            bot.sendMessage(chatId, message.ackready, removeKeyboard);
+            bot.sendMessage(chatId, message.ackreadyvideo, removeKeyboard);
 
             let url = callback.replace('1001-', '')
-            return wrapper.downloadVideo(url)
+            return wrapper.downloadVideo(chatId, url)
         }
     })
     .then((model) => {
+        const chatId = model.chatId;
         const type = model.type;
         const path = model.path;
 
         if (type == 'audio') {
-            bot.sendAudio()
+            bot.sendMessage(chatId, message.filereadyaudio)
+            bot.sendAudio(chatId, path)
         } else {
-            
+            bot.sendMessage(chatId, message.filereadyvideo)
+            bot.sendVideo(chatId, path)
         }
-    });
+    })
+    .catch((err) => {
+        console.log(err)
+        bot.sendMessage('47220554', `Something happen weird... log: ${err}`)
+    })
 
 
 exports.bot = this.bot
