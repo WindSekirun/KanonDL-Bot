0c7a9ce9fcca7e24269ecb9596e68f0e9e9a266f
Pyxis <pyxis@uzuki.live>
2019-10-12 01:42:44 +0900

    change start message

--- src/core/botcommand.ts
+++ src/core/botcommand.ts
@@ -1,4 +1,5 @@
 import TelegramBot = require("node-telegram-bot-api");
+import * as settings from '../../settings.json';
 
 export abstract class BotCommand {
     abstract matchRegex: RegExp;
@@ -7,6 +8,10 @@ export abstract class BotCommand {
 
     textMatch?(bot: TelegramBot): void {
         bot.onText(this.matchRegex, (message: TelegramBot.Message, matchArray: RegExpMatchArray) => {
+            if (settings.DEBUG_MODE && settings.LOG_MESSAGE_BODY) {
+                console.log(message)
+            }
+
             this.onMatch(message, matchArray)
         }) 
     }
