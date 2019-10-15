141f0d27081ae16869c3610125a64f0f06957173
Pyxis <pyxis@uzuki.live>
2019-10-15 20:54:50 +0900

    fix undefind.toBoolean()

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,7 +1,7 @@
 import '../core/ext/string'
 
-export let BOT_TOKEN = process.env.BOT_TOKEN
-export let ADMIN_USER_ID = process.env.ADMIN_USER_ID
-export let DEBUG_MODE: boolean = process.env.DEBUG_MODE.toBoolean()
-export let LOG_MESSAGE_BODY: boolean = process.env.LOG_MESSAGE_BODY.toBoolean()
-export let TIMEOUT_MILLIS: number = +process.env.TIMEOUT_MILLIS
+export let BOT_TOKEN = process.env.BOT_TOKEN || ""
+export let ADMIN_USER_ID = process.env.ADMIN_USER_ID || ""
+export let DEBUG_MODE: boolean = (process.env.DEBUG_MODE || "").toBoolean()
+export let LOG_MESSAGE_BODY: boolean = (process.env.LOG_MESSAGE_BODY || "").toBoolean()
+export let TIMEOUT_MILLIS: number = +(process.env.TIMEOUT_MILLIS || "")
